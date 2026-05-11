89b7636e6e93e80689fd9073475fe63a804d7782
doudouhule <commandblock128@qq.com>
2026-05-11 16:14:41 +0800

    Site updated: 2026-05-11 16:14:41

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/05/05/网页设计/如何部署自己的hexo博客/","2026/05/05/hello-world/"];function toRandomPost(){
+var posts=["2026/05/05/hello-world/","2026/05/05/网页设计/如何部署自己的hexo博客/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
